0c84c94629c8dcff62be2a02c983ee8ea3bd9271
Filippo Benozzi <filippobenozzi@gmail.com>
2020-03-16 16:57:02 +0100

    implementazione slider

--- src/pages/servizi.jsx
+++ src/pages/servizi.jsx
@@ -4,8 +4,85 @@ import Meta from 'components/meta'
 import { siteMetadata } from '../../gatsby-config'
 import Layout from 'components/layout'
 import Lines from 'components/lines'
+import Swiper from 'swiper'
 
 class Servizi extends React.Component{
+
+  constructor() {
+    super()
+    this.page = new React.createRef()
+    this.fullScreenSlider = new React.createRef()
+    this.sliderIside = new React.createRef()
+  }
+
+  initSlider(){
+
+    const swiper = new Swiper(this.fullScreenSlider.current, {
+      direction: 'vertical',
+      slidesPerView: 1,
+      spaceBetween: 0,
+      mousewheel: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    })
+
+    const swiper2 = new Swiper(this.sliderIside.current, {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      mousewheel: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    })
+
+    swiper.on('slideChange', function () {
+      swiper.mousewheel.disable()
+    })
+
+    swiper2.on('slideChangeTransitionEnd', function () {
+      if(swiper2.isEnd) {
+        swiper.mousewheel.enable()
+      }
+    })
+
+    swiper2.on('reachBeginning', function () {
+      swiper.mousewheel.enable()
+    })
+
+  }
+
+  initFullScreenScroll(){
+
+    const target = this.fullScreenSlider.current
+    let counterSlide = 0
+
+    window.addEventListener('wheel', function(event){
+      if (event.wheelDelta < 0) {
+        console.log('scrolling up', counterSlide)
+        counterSlide -= event.wheelDelta
+        target.scrollTop = counterSlide
+      }
+      else if (event.wheelDelta > 0) {
+        console.log('scrolling down', counterSlide);
+        counterSlide += event.wheelDelta
+        target.scrollTop = counterSlide
+      }
+    })
+
+  }
+
+  componentDidMount() {
+    this.initSlider()
+    this.initFullScreenScroll()
+  }
+
+  componentDidUpdate() {
+    this.initSlider()
+    this.initFullScreenScroll()
+  }
 
   render() {
 
@@ -13,7 +90,7 @@ class Servizi extends React.Component{
 
     return(
 
-      <div className={style.page}>
+      <div ref={this.page} className={style.page}>
         <Layout location={location}>
           <Meta site={siteMetadata} title="Servizi" />
 
@@ -21,13 +98,39 @@ class Servizi extends React.Component{
 
           <div className={`container-fluid ${style.page__content}`}>
             <div className={`row`}>
-              <div className={`offset-md-2 col-md-3`}>
+              <div className={`offset-md-2 col-md-2`}>
                 <div className={style.page__content__menu}>
                   Test
                 </div>
               </div>
-              <div className={`col-md-7`}>
+              <div className={`offset-md-1 col-md-7 no-padding ${style.page__content__fullScreenSlide}`}>
+                <div ref={this.fullScreenSlider} className="swiper-container">
+                  <div className="swiper-wrapper">
+                    <div className={`swiper-slide ${style.testSliderOutside}`} style={{ backgroundColor: 'green', color: 'white' }} >Slide 1</div>
+                    <div className={`swiper-slide ${style.testSliderOutside}`} style={{ backgroundColor: 'blue', color: 'white' }}>
 
+                      <div ref={this.sliderIside} className="swiper-container">
+                        <div className="swiper-wrapper">
+                          <div className={`swiper-slide ${style.testSliderInside}`} style={{ backgroundColor: 'red', color: 'white' }}>Slide 1</div>
+                          <div className={`swiper-slide ${style.testSliderInside}`}  style={{ backgroundColor: 'green', color: 'white' }}>Slide 2</div>
+                          <div className={`swiper-slide ${style.testSliderInside}`}  style={{ backgroundColor: 'yellow', color: 'white' }}>Slide 3</div>
+                          <div className={`swiper-slide ${style.testSliderInside}`}  style={{ backgroundColor: 'orange', color: 'white' }}>Slide 3</div>
+                          <div className={`swiper-slide ${style.testSliderInside}`}  style={{ backgroundColor: 'black', color: 'white' }}>Slide 3</div>
+                          <div className={`swiper-slide ${style.testSliderInside}`} style={{ backgroundColor: 'red', color: 'white' }}>Slide 1</div>
+                          <div className={`swiper-slide ${style.testSliderInside}`}  style={{ backgroundColor: 'green', color: 'white' }}>Slide 2</div>
+                          <div className={`swiper-slide ${style.testSliderInside}`}  style={{ backgroundColor: 'yellow', color: 'white' }}>Slide 3</div>
+                          <div className={`swiper-slide ${style.testSliderInside}`}  style={{ backgroundColor: 'orange', color: 'white' }}>Slide 3</div>
+                          <div className={`swiper-slide ${style.testSliderInside}`}  style={{ backgroundColor: 'black', color: 'white' }}>Slide 3</div>
+                        </div>
+                        <div className="swiper-pagination"></div>
+                      </div>
+
+                    </div>
+                    <div className={`swiper-slide ${style.testSliderOutside}`} style={{ backgroundColor: 'yellow', color: 'white' }}>Slide 3</div>
+                    <div className={`swiper-slide ${style.testSliderOutside}`} style={{ backgroundColor: 'orange', color: 'white' }}>Slide 4</div>
+                  </div>
+                  <div className="swiper-pagination"></div>
+                </div>
               </div>
             </div>
           </div>
